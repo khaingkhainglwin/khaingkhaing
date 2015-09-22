0a9a7ef29cfa97516f9d1d7e6ad2373d9299044e
khaingkhainglwin <your.khaingkhainglwin@cloudsource.co.jp>
2015-09-22 14:23:40 +0630

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
     get 'static_pages/about'
-
+  get  'static_pages/contact'
+    get 'signup'  => 'users#new'
   resources :microposts
   resources :users
     root 'users#index'
